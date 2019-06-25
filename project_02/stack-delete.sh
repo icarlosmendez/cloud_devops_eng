2111287d373d5e53c93a3da169f82537f6cf77fa
Carlos Mendez <calicci.gardachi@gmail.com>
2019-06-25 12:10:51 -1000

    added stack-delete script

---
+++ project_02/stack-delete.sh
@@ -0,0 +1,9 @@
+# This script will execute the deletion of an AWS stack
+
+# Using a relative file path from inside the directory containing the script file,
+# call the script and pass in the stack-name
+
+# ./stack-create.sh <stack name used during creation>
+
+aws cloudformation delete-stack \
+--stack-name $1
